a5e09bb45d2e9370dec5ed74fe93c5f8295ef144
cc0803 <simongeldbach@gmail.com>
2023-06-27 17:09:07 +0200

    Add backwards animation

--- app.js
+++ app.js
@@ -6,13 +6,25 @@ const listIcons = Array.from(document.querySelectorAll("li i"));
 
 menuButton.addEventListener("click", () => {
 	menuButtonIcon.classList.add("rotate");
+	menuButtonIcon.classList.remove("rotateReversed");
 	list.removeAttribute("id", "invisible");
 	listIcons.forEach((icon) => {
 		icon.removeAttribute("id", "invisible");
 	});
+
 	setTimeout(() => {
-		menuButton.setAttribute("id", "invisible");
+		menuButton.classList.add("z-index-shift");
 	}, 500);
 });
 
-closeButton.addEventListener("click", () => {});
+closeButton.addEventListener("click", () => {
+	menuButton.removeAttribute("id", "invisible");
+	menuButton.classList.remove("z-index-shift");
+	menuButtonIcon.classList.remove("rotate");
+
+	list.setAttribute("id", "invisible");
+
+	listIcons.forEach((icon) => {
+		icon.setAttribute("id", "invisible");
+	});
+});
